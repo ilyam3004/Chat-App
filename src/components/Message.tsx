66cb11f1e9b44deb92f389f3366b30576b8ec1c0
ilyam3004 <ilyamelnichuk3004@gmail.com>
2023-01-15 23:54:54 +0200

    Remaking design and configuring sending messages

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,42 +1,54 @@
 import React, {FC} from 'react';
-import {IMessage} from "../types/types";
-import {HubConnection} from "@microsoft/signalr";
+import {IMessage, IUser} from "../types/types";
 import "../App.scss";
 
 interface MessageProps {
     message: IMessage;
-    connection: HubConnection;
+    user: IUser;
 }
 
-export const Message: FC<MessageProps> = ({message, connection}) => {
+export const Message: FC<MessageProps> = ({message, user}) => {
     return (
         <div>
             {
-                message.connectionId === connection.connectionId
+                message.fromUser
                     ?
-                    <div className="message owner">
-                        <div className="message-info">
-                            <span>{message.username}</span>
-                        </div>
-                        <div className="message-content">
-                            <p>{message.text}</p>
-                        </div>
-                        <div className="message-date">
-                            {message.date.toLocaleDateString()}
-                        </div>
-                    </div>
+                    (
+                        message.userId === user.userId
+                            ?
+                            <div className="message owner">
+                                <div className="message-info">
+                                    <span>{message.username}</span>
+                                </div>
+                                <div className="message-content">
+                                    <p>{message.text}</p>
+                                </div>
+                                <div className="message-date">
+                                    {message.date.toString()}
+                                </div>
+                            </div>
+                            :
+                            (<div className={'message'}>
+                                <div className={'message-info'}>
+                                    <span>{message.username}</span>
+                                </div>
+                                <div className={'message-content'}>
+                                    <p>{message.text}</p>
+                                </div>
+                                <div className="message-date">
+                                    {message.date.toString()}
+                                </div>
+                            </div>)
+                    )
                     :
-                    (<div className={'message'}>
-                        <div className={'message-info'}>
-                            <span>{message.username}</span>
+                    (
+                        <div className="message from-server">
+                            <div className="message-content">
+                                <p>{message.text}</p>
+                            </div>
                         </div>
-                        <div className={'message-content'}>
-                            <p>{message.text}</p>
-                        </div>
-                        <div className="message-date">
-                            {message.date.toLocaleString()}
-                        </div>
-                    </div>)
+                    )
+
             }
         </div>
     );
