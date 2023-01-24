240c5aef1b8d6c64b9344256d67860ec43ede36c
ilyam3004 <ilyamelnichuk3004@gmail.com>
2023-01-24 16:13:02 +0200

    Fix showing errors while login and adding loading to roompage

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -1,7 +1,7 @@
 import React, {FC, FormEvent, useEffect, useState} from "react";
 import {IError, IJoinRoomRequest} from "../types/types";
-import '../App.scss';
 import {MoonLoader} from "react-spinners";
+import '../App.scss'
 
 interface LobbyProps {
     joinRoom: (request: IJoinRoomRequest) => void;
@@ -12,7 +12,7 @@ interface LobbyProps {
 export const Lobby: FC<LobbyProps> = ({joinRoom, error, setError}) => {
 
     const [loading, setLoading] = useState(false);
-
+    const delayInSeconds: number = 2;
     const [values, setValues] = useState<IJoinRoomRequest>({username: '', roomName: ''});
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -29,19 +29,25 @@ export const Lobby: FC<LobbyProps> = ({joinRoom, error, setError}) => {
     }
 
     const getError = (): string => {
-        if(error){
-            return error.errors.Username !== undefined
-                ? error.errors.Username[0]
-                : error.errors.RoomName[0];
+        if (error) {
+            if (error.errors != undefined) {
+                return error.errors.Username !== undefined
+                    ? error.errors.Username[0]
+                    : error.errors.RoomName[0];
+            }
+            return error.title;
         }
         return "";
     }
 
     useEffect(() => {
-        if(error){
-            setLoading(false);
+        if (error) {
+            let timer = setTimeout(() => setLoading(false), delayInSeconds * 400)
+            return () => {
+                clearTimeout(timer);
+            };
         }
-    }, [error?.errors]);
+    }, [error]);
 
 
     return (
@@ -53,11 +59,16 @@ export const Lobby: FC<LobbyProps> = ({joinRoom, error, setError}) => {
                     error
                         ?
                         (
-                            <div className="error-container">
-                                <div className="error-text">
-                                    {getError()}
-                                </div>
-                            </div>
+                            !loading
+                                ?
+                                    (
+                                        <div className="error-container">
+                                            <div className="error-text">
+                                                {getError()}
+                                            </div>
+                                        </div>
+                                    )
+                                : <div></div>
                         )
                         :
                         <div></div>
