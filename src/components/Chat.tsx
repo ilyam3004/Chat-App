de893f51d2bec3216b8bf620a3da1cc8e81bf89d
ilyam3004 <ilyamelnichuk3004@gmail.com>
2023-01-26 22:43:29 +0200

    Remaking app design

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -5,17 +5,25 @@ import {HubConnection} from "@microsoft/signalr";
 import {IMessage, IUser} from "../types/types";
 import "../App.scss";
 
-interface ChatProps{
+interface ChatProps {
     connection: HubConnection;
     messages: IMessage[];
     userData: IUser;
+    membersCount: number;
 }
 
-export const Chat: FC<ChatProps> = ({messages, userData, connection}) => {
+export const Chat: FC<ChatProps> = ({messages, userData, connection, membersCount}) => {
     return (
         <div className="chat">
             <div className="chat-info">
-                <span className="chat-name">{userData.roomName}</span>
+                <span>{userData.roomName}</span>
+                {
+                    membersCount > 1
+                        ?
+                            <div className="members-count">{membersCount} members</div>
+                        :
+                            <div className="members-count">{membersCount} member</div>
+                }
             </div>
             <Messages messages={messages}
                       user={userData}/>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,6 +20,8 @@ export const Navbar: FC<NavbarProps> = ({closeConnection, userData}) => {
         <div className="navbar">
             <span className="logo">Chat app</span>
             <div className="user">
+                <img src="https://media.istockphoto.com/id/1131164548/vector/avatar-5.jpg?s=612x612&w=0&k=20&c=CK49ShLJwDxE4kiroCR42kimTuuhvuo2FH5y_6aSgEo="
+                     alt=""/>
                 <span>{userData.username}</span>
                 <button className="log-out-button"
                         onClick={logOut}>Logout
