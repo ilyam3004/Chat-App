b16b5550edcd9b4fc4b01be88459d13354b1398c
ilyam3004 <ilyamelnichuk3004@gmail.com>
2023-02-07 23:05:40 +0200

    Modify components to sending images

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,4 +1,4 @@
-import React, {FormEvent, useRef, FC, useState} from 'react';
+import React, {FormEvent, useRef, FC, useState, ChangeEvent} from 'react';
 import "../App.scss";
 import {HubConnection} from "@microsoft/signalr";
 
@@ -9,25 +9,50 @@ interface ChatInputProps{
 export const ChatInput: FC<ChatInputProps> = ({connection}) => {
 
     const messageRef = useRef<HTMLInputElement>(null);
+    const fileRef = useRef<HTMLInputElement>(null);
     const [count, setCount] = useState<number>(0);
+    const [selectedFile, setSelectedFile] = useState<File>();
 
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setCount(0);
+        if(selectedFile){
+            const formData = new FormData();
+            formData.append('File', selectedFile, selectedFile.name);
+            console.log(formData)
+            await sendImage(formData);
+            setSelectedFile(undefined);
+        }
         if(messageRef.current){
             await sendMessage(messageRef.current.value);
             messageRef.current.value = '';
         }
     }
 
-    const onChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const onTextInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault();
         setCount(e.target.value.length);
+    }
+
+    const onFileInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if(e.target.files){
+            console.log(e.target.files[0]);
+            console.log(fileRef.current?.files)
+            setSelectedFile(e.target.files[0]);
+        }
     }
 
     const sendMessage = async (message: string) => {
         try {
             await connection.invoke("SendUserMessage", message);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const sendImage = async (image: FormData) => {
+        try {
+            await connection.invoke("GetUserDataAndSendImage", image);
         } catch (e) {
             console.log(e);
         }
@@ -40,12 +65,15 @@ export const ChatInput: FC<ChatInputProps> = ({connection}) => {
                 type="text"
                 ref={messageRef}
                 placeholder="Type message..."
-                onChange={onChange}/>
+                onChange={onTextInputChange}/>
             <div className="counter" style={{ color: count > 150 ? "#f17c7c" : "#a9a7a7" }}>
                 {count}/150
             </div>
+            <input type="file"
+                   ref={fileRef}
+                   onChange={onFileInputChange}/>
             <div className="send">
-                <button type="submit" disabled={count > 150 || !messageRef.current?.value}>Send</button>
+                <button type="submit" disabled={!fileRef.current?.files}>Send</button>
             </div>
         </form>
     );
