783b03e8d48eaf8ea8088e12aa1d0446e230d2f0
ilyam3004 <ilyamelnichuk3004@gmail.com>
2023-02-08 21:26:28 +0200

    Adding reuqest for uploadinig images

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,8 +1,9 @@
 import React, {FormEvent, useRef, FC, useState, ChangeEvent} from 'react';
-import "../App.scss";
 import {HubConnection} from "@microsoft/signalr";
+import "../App.scss";
+import axios from "axios";
 
-interface ChatInputProps{
+interface ChatInputProps {
     connection: HubConnection;
 }
 
@@ -12,30 +13,27 @@ export const ChatInput: FC<ChatInputProps> = ({connection}) => {
     const fileRef = useRef<HTMLInputElement>(null);
     const [count, setCount] = useState<number>(0);
     const [selectedFile, setSelectedFile] = useState<File>();
+    const baseUrl = "http://localhost:5113/";
 
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setCount(0);
-        if(selectedFile){
-            const formData = new FormData();
-            formData.append('File', selectedFile, selectedFile.name);
-            console.log(formData)
-            await sendImage(formData);
-            setSelectedFile(undefined);
+        if (selectedFile) {
+            sendImage(selectedFile);
         }
-        if(messageRef.current){
-            await sendMessage(messageRef.current.value);
-            messageRef.current.value = '';
+        if (messageRef.current!.value.length > 0) {
+            await sendMessage(messageRef.current!.value);
+            messageRef.current!.value = '';
         }
     }
 
-    const onTextInputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    const onTextInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault();
         setCount(e.target.value.length);
     }
 
     const onFileInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        if(e.target.files){
+        if (e.target.files) {
             console.log(e.target.files[0]);
             console.log(fileRef.current?.files)
             setSelectedFile(e.target.files[0]);
@@ -50,13 +48,24 @@ export const ChatInput: FC<ChatInputProps> = ({connection}) => {
         }
     }
 
-    const sendImage = async (image: FormData) => {
-        try {
-            await connection.invoke("GetUserDataAndSendImage", image);
-        } catch (e) {
-            console.log(e);
-        }
-    }
+    const sendImage = async (image: File) => {
+        let data = new FormData();
+        data.append('File', image);
+        const url = `${baseUrl}img/uploadImage`;
+        await fetch(url,
+            {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Accept': 'multipart/form-data',
+                },
+                body: data
+            }).then((response) => {
+            console.log(response.body)
+        }).catch((error) => {
+            console.log(error);
+        });
+    };
 
     return (
         <form className="chat-input"
@@ -66,15 +75,18 @@ export const ChatInput: FC<ChatInputProps> = ({connection}) => {
                 ref={messageRef}
                 placeholder="Type message..."
                 onChange={onTextInputChange}/>
-            <div className="counter" style={{ color: count > 150 ? "#f17c7c" : "#a9a7a7" }}>
+            <div className="counter" style={{color: count > 150 ? "#f17c7c" : "#a9a7a7"}}>
                 {count}/150
             </div>
             <input type="file"
                    ref={fileRef}
                    onChange={onFileInputChange}/>
             <div className="send">
-                <button type="submit" disabled={!fileRef.current?.files}>Send</button>
+                <button type="submit"
+                        disabled={!fileRef.current?.files && !messageRef.current?.value || count > 150}>
+                    Send
+                </button>
             </div>
         </form>
     );
-};
+}
