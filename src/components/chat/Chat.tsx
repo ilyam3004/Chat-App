437684903cc6decb7ae515b185eed8c9ec315a7e
ilyam3004 <ilyamelnichuk3004@gmail.com>
2023-02-18 21:10:55 +0200

    Fixing bug with auto scrolling on image message

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,9 +1,9 @@
 import React, {FC} from 'react';
-import {Messages} from "./Messages";
-import {ChatInput} from "./ChatInput";
+import {AllMessages} from "./AllMessages";
+import {ChatInput} from "../inputs/ChatInput";
 import {HubConnection} from "@microsoft/signalr";
-import {IMessage, IUser} from "../types/types";
-import "../App.scss";
+import {IMessage, IUser} from "../../types/types";
+import "../../App.scss";
 
 interface ChatProps {
     connection: HubConnection;
@@ -25,8 +25,8 @@ export const Chat: FC<ChatProps> = ({messages, userData, connection, membersCoun
                             <div className="members-count">{membersCount} member</div>
                 }
             </div>
-            <Messages messages={messages}
-                      user={userData}/>
+            <AllMessages messages={messages}
+                         user={userData}/>
             <ChatInput connection={connection}
                        userData={userData}/>
         </div>
--- src/components/chat/ServerMessage.tsx
+++ src/components/chat/ServerMessage.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react';
-import {IMessage} from "../types/types";
+import {IMessage} from "../../types/types";
 
 interface ServerMessageProps{
     message: IMessage;
