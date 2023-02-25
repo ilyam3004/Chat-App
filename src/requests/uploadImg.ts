f6d8413313ac2311d7636b581b88567ab6ddc948
ilyam3004 <ilyamelnichuk3004@gmail.com>
2023-02-25 23:19:48 +0200

    Changing request to upload avatars

--- src/requests/uploadImg.ts
+++ src/requests/uploadImg.ts
@@ -19,3 +19,18 @@ export async function uploadImg(image: File):Promise<IUploadResult> {
 
     return result;
 }
+
+export async function uploadAvatar(avatar: File):Promise<IUploadResult> {
+    let data = new FormData();
+    data.append('avatar', avatar);
+
+    const url = `${baseUrl}img/uploadAvatar`;
+
+    const result = await axios.post(url, data).then((response) => {
+        return response.data;
+    }).catch((error) => {
+        console.log(error);
+    });
+
+    return result;
+}
